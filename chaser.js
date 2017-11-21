8ce722f41583f78275a340b43a72d9bd67270e3a
Jenna Berlinberg <jenna.berlinberg@gmail.com>
2017-11-21 15:18:19 -0800

    update code with multiple enemies

--- chaser.js
+++ chaser.js
@@ -1,41 +1,43 @@
 const canvas = document.querySelector("canvas");
 const ctx = canvas.getContext("2d");
 
-let ball = { x: 250, y: 150, dx: 3, dy: 5, radius: 20, color: "lemonchiffon" };
+let ball = { x: 250, y: 150, radius: 25, color: "lemonchiffon" };
+let mouse = { x: 0, y: 0 };
 
 class Enemy {
   constructor(x, y, width, color) {
-    Object.assign(this, {x,y,width,color});
-  };
-};
+    Object.assign(this, {x, y, width, color});
+  }
+}
 
 let enemies = [
-  new Enemy(80,200,20,'crimson'),
-  new Enemy(200,110,25,'orange'),
-  new Enemy(102,283,17,'purple')
+  new Enemy(80, 200, 20, 'crimson'),
+  new Enemy(200, 250, 17, 'orange'),
+  new Enemy(150, 180, 22, 'mauve'),
 ];
 
-let mouse = { x: 0, y: 0 };
 function updateMouse(event) {
   const { left, top } = canvas.getBoundingClientRect();
   mouse.x = event.clientX - left;
   mouse.y = event.clientY - top;
 }
+
 document.body.addEventListener("mousemove", updateMouse);
+
 function clearBackground() {
-  ctx.fillStyle = "cadetblue";
+  ctx.fillStyle = "lightgreen";
   ctx.fillRect(0, 0, canvas.width, canvas.height);
 }
+
 function drawBall() {
   ctx.fillStyle = ball.color;
   ctx.beginPath();
   ctx.arc(ball.x, ball.y, ball.radius, 0, Math.PI * 2);
-  ctx.strokeStyle = "royalblue";
   ctx.fill();
-  ctx.lineWidth = 2;
   ctx.stroke();
 }
-function drawEnemy() {
+
+function drawEnemy(enemy) {
   ctx.fillStyle = enemy.color;
   ctx.fillRect(
     enemy.x - enemy.width / 2,
@@ -51,30 +53,23 @@ function drawEnemy() {
   );
 }
 
-function updateScene() {
-  ball.x -= ball.dx;
-  ball.y -= ball.dy;
-  if (ball.y <= ball.radius || ball.y >= canvas.height - ball.radius) {
-    ball.dy *= -1;
-  }
-  if (ball.x <= ball.radius || ball.x >= canvas.height - ball.radius) {
-    ball.dx *= -1;
-  }
-}
-function moveToward(leader, follower, speed){
+function moveToward(leader, follower, speed) {
   follower.x += (leader.x - follower.x) * speed;
   follower.y += (leader.y - follower.y) * speed;
 }
-function updateSceneWithMouse() {
-  moveToward(mouse,ball,0.2);
-  moveToward(ball,enemy,0.05)
+
+function updateScene() {
+  moveToward(mouse, ball, 0.05);
+  enemies.forEach(enemy => moveToward(ball, enemy, 0.02));
 }
-let x = 250;
+
 function drawScene() {
   clearBackground();
   drawBall();
-  drawEnemy();
-  updateSceneWithMouse();
+
+  enemies.forEach(drawEnemy);
+
+  updateScene();
   requestAnimationFrame(drawScene);
 }
 
