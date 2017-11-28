cc6ab310bec3de6ff1cab2b8ae251dfcb346f1b9
Luis Garcia <luisfergar99@gmail.com>
2017-11-28 15:06:38 -0800

    adds progress bar

--- chaser.js
+++ chaser.js
@@ -1,61 +1,71 @@
 const canvas = document.querySelector("canvas");
 const ctx = canvas.getContext("2d");
+const progressBar = document.querySelector("progress");
+
+function distanceBetween(sprite1, sprite2) {
+  return Math.hypot(sprite1.x - sprite2.x, sprite1.y - sprite2.y);
+}
+
+function haveCollided(sprite1, sprite2) {
+  return distanceBetween(sprite1, sprite2) < sprite1.radius + sprite2.radius;
+}
 
 class Sprite {
   draw() {
     ctx.fillStyle = this.color;
     ctx.beginPath();
     ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2);
+    ctx.strokeStyle = "indianred";
     ctx.fill();
+    ctx.lineWidth = 2;
     ctx.stroke();
   }
 }
-
 class Player extends Sprite {
   constructor(x, y, radius, color, speed) {
     super();
-    Object.assign(this, {x, y, radius, color, speed});
+    Object.assign(this, { x, y, radius, color, speed });
   }
 }
-
-let player = new Player(250, 150, 15, 'lemonchiffon', 0.07);
+let player = new Player(250, 150, 20, "lemonchiffon", 0.07);
 
 class Enemy extends Sprite {
   constructor(x, y, radius, color, speed) {
     super();
-    Object.assign(this, {x, y, radius, color, speed});
+    Object.assign(this, { x, y, radius, color, speed });
   }
 }
 
 let enemies = [
-  new Enemy(80, 200, 20, 'rgba(250, 0, 50, 0.8)', 0.02),
-  new Enemy(200, 250, 17, 'rgba(200, 100, 0, 0.7)', 0.01),
-  new Enemy(150, 180, 22, 'rgba(50, 10, 70, 0.5)', 0.002),
+  new Enemy(80, 200, 20, "rgba(250,1,180,0.9)", 0.02),
+  new Enemy(200, 250, 17, "rgba(50,200,70,0.7)", 0.01),
+  new Enemy(150, 180, 22, "rgba(80,200,190,0.4)", 0.05)
 ];
 
-
 let mouse = { x: 0, y: 0 };
-document.body.addEventListener("mousemove", updateMouse);
 function updateMouse(event) {
   const { left, top } = canvas.getBoundingClientRect();
   mouse.x = event.clientX - left;
   mouse.y = event.clientY - top;
 }
-
+document.body.addEventListener("mousemove", updateMouse);
+function clearBackground() {
+  ctx.fillStyle = "cadetblue";
+  ctx.fillRect(0, 0, canvas.width, canvas.height);
+}
 
 function moveToward(leader, follower, speed) {
   follower.x += (leader.x - follower.x) * speed;
   follower.y += (leader.y - follower.y) * speed;
 }
-
 function updateScene() {
   moveToward(mouse, player, player.speed);
   enemies.forEach(enemy => moveToward(player, enemy, enemy.speed));
-}
-
-function clearBackground() {
-  ctx.fillStyle = "lightgreen";
-  ctx.fillRect(0, 0, canvas.width, canvas.height);
+  enemies.forEach(enemy => {
+    if (haveCollided(enemy, player)) {
+      progressBar.value -= 2;
+    }
+  });
 }
 
 function drawScene() {
@@ -63,7 +73,11 @@ function drawScene() {
   player.draw();
   enemies.forEach(enemy => enemy.draw());
   updateScene();
-  requestAnimationFrame(drawScene);
+  if (progressBar.value <= 0) {
+   alert('Game over');
+  } else {
+    requestAnimationFrame(drawScene);
+  }
 }
 
 requestAnimationFrame(drawScene);
