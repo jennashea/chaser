1cf0b04273a8c9b86e940251ae72a97c3746bcde
Jenna Berlinberg <jenna.berlinberg@gmail.com>
2017-12-08 22:25:30 -0800

    fix infinite enemy spawn

--- chaser.js
+++ chaser.js
@@ -271,6 +271,10 @@ const score = setInterval(IncreaseScore, 1000);
 requestAnimationFrame(drawScene);
 function restartGame() {
   if (progressBar.value === 0) {
+    for (const powerUp in powerUps) {
+      powerUps[powerUp].objects=[];
+    }
+    enemies=[];
     progressBar.value = 100;
     Object.assign(player, { x: canvas.width / 2, y: canvas.height / 2 });
     requestAnimationFrame(drawScene);
