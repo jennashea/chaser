3259bdf304d3293305d21a1dbb206013cfcaa61c
MiscLG <luisfergar99@gmail.com>
2017-12-08 21:58:05 -0800

    button does something

--- chaser.js
+++ chaser.js
@@ -3,29 +3,28 @@ const ctx = canvas.getContext("2d");
 const progressBar = document.querySelector("progress");
 const button = document.querySelector("button");
 const defaultCharacterDimensions = 64;
-const defaultPowerUpDimensions =64;
+const defaultPowerUpDimensions = 64;
 const playerDimensions = 64;
-const enemyDimensions = 64;
 let points = 0;
 let scoreMultiplier = 1;
-let drawHitBox =false;
-let time = 0;
+let drawHitBox = false;
 
 function distanceBetween(sprite1, sprite2) {
   return Math.hypot(sprite1.x - sprite2.x, sprite1.y - sprite2.y);
 }
+         
 class Sprite {
   draw() {
-      if (drawHitBox) {
-          ctx.fillStyle = this.color;
-          ctx.beginPath();
-          ctx.arc(this.x, this.y, this.hitBoxRadius, 0, Math.PI * 2);
-          ctx.strokeStyle = "salmon";
-          ctx.fill();
-          ctx.lineWidth = 2;
-          ctx.stroke();
-        }
-      ctx.drawImage(
+    if (drawHitBox) {
+      ctx.fillStyle = this.color;
+      ctx.beginPath();
+      ctx.arc(this.x, this.y, this.hitBoxRadius, 0, Math.PI * 2);
+      ctx.strokeStyle = "salmon";
+      ctx.fill();
+      ctx.lineWidth = 2;
+      ctx.stroke();
+    } 
+    ctx.drawImage(
       this.image,
       this.x - this.dimensions / this.centerLineFraction,
       this.y - this.dimensions / 2,
@@ -38,7 +37,7 @@ class Sprite {
       distanceBetween(this, sprite2) < this.hitBoxRadius + sprite2.hitBoxRadius
     );
   }
-    pushOff(sprite2) {
+  pushOff(sprite2) {
     let [dx, dy] = [sprite2.x - this.x, sprite2.y - this.y];
     const L = Math.hypot(dx, dy);
     let distToMove = this.hitBoxRadius + sprite2.hitBoxRadius - L;
@@ -50,9 +49,8 @@ class Sprite {
       sprite2.x += dx * distToMove / 2;
       sprite2.y += dy * distToMove / 2;
     }
-  }
-}
-
+  } 
+} 
 class Player extends Sprite {
   constructor(x, y, hitBoxRadius, color, speed) {
     super();
@@ -60,10 +58,10 @@ class Player extends Sprite {
     this.image.src =
       "http://res.cloudinary.com/misclg/image/upload/v1512342979/mikuSprite_Left.png";
     this.dimensions = defaultCharacterDimensions;
-    this.centerLineFraction=3;
+    this.centerLineFraction = 3;
     Object.assign(this, { x, y, hitBoxRadius, color, speed });
   }
-    pushOff(sprite2) {
+  pushOff(sprite2) {
     let [dx, dy] = [sprite2.x - this.x, sprite2.y - this.y];
     const distance = this.hitBoxRadius + sprite2.hitBoxRadius;
     if (this.hasCollided(sprite2)) {
@@ -88,23 +86,21 @@ class Enemy extends Sprite {
     this.image.src =
       "http://res.cloudinary.com/randomstuff/image/upload/v1512513576/VirusSprite_qmtkaq.png";
     this.dimensions = defaultCharacterDimensions;
-    this.centerLineFraction =2;
+    this.centerLineFraction = 2;
     Object.assign(this, { x, y, hitBoxRadius, color, speed });
   }
 }
 
 let enemies = [];
-
 function findIndexOfFastestEnemy() {
   let enemySpeeds = enemies.map(enemy => enemy.speed);
   return enemySpeeds.indexOf(Math.max(...enemySpeeds));
 }
-
 class PowerUp extends Sprite {
-    deleteSelf() {
-        let eraseIndex = powerUps[this.powerUpType].objects.indexOf(this);
-        powerUps[this.powerUpType].objects.splice(eraseIndex, 1);
-    }
+  deleteSelf() {
+    let eraseIndex = powerUps[this.powerUpType].objects.indexOf(this);
+    powerUps[this.powerUpType].objects.splice(eraseIndex, 1);
+  }
 }
 
 const powerUps = {
@@ -121,7 +117,7 @@ class ScoreFactor extends PowerUp {
     this.image.src =
       "http://res.cloudinary.com/misclg/image/upload/v1512363142/PowerUpSprite_Leek.png";
     this.dimensions = defaultPowerUpDimensions;
-    this.centerLineFraction=2;
+    this.centerLineFraction = 2;
     this.powerUpType = "scoreFactor";
   }
   activate() {
@@ -133,7 +129,7 @@ class EnemyEraser extends PowerUp {
   constructor(x, y, hitBoxRadius, color) {
     super();
     Object.assign(this, { x, y, hitBoxRadius, color });
-      this.image = new Image();
+    this.image = new Image();
     this.image.src =
       "http://res.cloudinary.com/misclg/image/upload/v1512616230/PowerUpSprite_Harisen.png";
     this.dimensions = defaultPowerUpDimensions;
@@ -141,11 +137,10 @@ class EnemyEraser extends PowerUp {
     this.powerUpType = "enemyEraser";
   }
   activate() {
-    enemies.splice(findIndexOfFastestEnemy(), 1);
+    enemies.splice(findIndexOfFastestEnemy(), 3);
+    
   }
 }
-
-
 class Health extends PowerUp {
   constructor(x, y, hitBoxRadius, color) {
     super();
@@ -158,45 +153,9 @@ class Health extends PowerUp {
     this.powerUpType = "health";
   }
   activate() {
-    progressBar.value += 10;
+    progressBar.value += 12.5;
   }
 }
-
-
-// class freezeEnemy extends PowerUp {
-//   constructor(x, y, hitBoxRadius, color) {
-//     super();
-//     Object.assign(this, { x, y, hitBoxRadius, color });
-//     this.image = new Image();
-//     this.image.src =
-//       "http://res.cloudinary.com/misclg/image/upload/v1512623984/PowerUpSprite_Snowflake.png";
-//     this.dimensions = defaultPowerUpDimensions;
-//     this.centerLineFraction=2;
-//   }
-//   activate() {
-//     let currentTime = time;
-//     while(time<= (currentTime+10)){
-//       enemies.forEach(enemy => enemy.speed =0);
-//       time++;
-//     }
-//   }
-//   erase() {
-//     let freezeIndex = freezeEnemies.indexOf(this);
-//     freezeEnemies.splice(freezeIndex, 1);
-//   }
-//   draw() {
-//     ctx.fillStyle = this.color;
-//     ctx.beginPath();
-//     ctx.arc(this.x, this.y, this.hitBoxRadius, 0, Math.PI * 2);
-//     ctx.strokeStyle = "sienna";
-//     ctx.fill();
-//     ctx.lineWidth = 2;
-//     ctx.stroke();
-//   }
-// }
-
-let freezeEnemies = [];
-
 let mouse = { x: 0, y: 0 };
 function updateMouse(event) {
   const { left, top } = canvas.getBoundingClientRect();
@@ -207,28 +166,28 @@ function updateMouse(event) {
 document.body.addEventListener("mousemove", updateMouse);
 
 function clearBackground() {
+  //ctx.fillStyle = rgba(220,220,220, 0.5);
   ctx.clearRect(0, 0, canvas.width, canvas.height);
-  ctx.fillStyle = "rgba(220,220,220, 0.8)";
-}
+  
+}   
 
 function moveToward(leader, follower, speed) {
   follower.x += (leader.x - follower.x) * speed;
   follower.y += (leader.y - follower.y) * speed;
-}
+} 
 function updateScene() {
   moveToward(mouse, player, player.speed);
   enemies.forEach(enemy => moveToward(player, enemy, enemy.speed));
   enemies.forEach(enemy => {
     if (enemy.hasCollided(player)) {
       progressBar.value -= 10;
-    }
+    } 
   });
   for (let i = 0; i < enemies.length; i++) {
     for (let j = i+1; j < enemies.length; j++) {
       enemies[i].pushOff(enemies[j]);
     }
     player.pushOff(enemies[i]);
-
   }
   const interact = function interactWithPlayer(object) {
     if (object.hasCollided(player)) {
@@ -239,30 +198,23 @@ function updateScene() {
   for (const powerUp in powerUps) {
     powerUps[powerUp].objects.forEach(interact);
   }
-
-  // freezeEnemies.forEach(freezeEnemy1 => {
-  //   if (freezeEnemy1.hasCollided(player)) {
-  //     freezeEnemy1.activate();
-  //     freezeEnemy1.erase();
-  //   }
-  // });
 }
+
 function endScene() {
   if (progressBar.value <= 0) {
-    ctx.font = "30px Arial";
-    ctx.fillText("Game over, click to play again", 0, canvas.height / 2);
+    //alert('Game over');
   } else {
     requestAnimationFrame(drawScene);
   }
 }
+
 function drawScene() {
   clearBackground();
   enemies.forEach(enemy => enemy.draw());
+  player.draw();
   for (const powerUp in powerUps) {
     powerUps[powerUp].objects.forEach(object => object.draw());
   }
-  // freezeEnemies.forEach(freezeEnemyI => freezeEnemyI.draw());
-  player.draw();
   updateScene();
   endScene();
 }
@@ -274,7 +226,7 @@ function spawnEnemy() {
       Math.random() * canvas.height,
       20,
       "blue",
-      Math.random() * (player.speed - player.speed / 30)
+      Math.random() * (player.speed - player.speed / (enemies.length%5===0?2:96))
     )
   );
 }
@@ -295,25 +247,13 @@ function createNewPowerUp(powerUpType) {
 function spawnScoreFactor() {
   createNewPowerUp(`scoreFactor`);
 }
-
+ 
 function spawnEnemyEraser() {
   createNewPowerUp("enemyEraser");
 }
 function spawnHealth() {
   createNewPowerUp("health");
 }
-
-function spawnFreeze() {
-  freezeEnemies.unshift(
-    new freezeEnemy(
-      Math.random() * canvas.width,
-      Math.random() * canvas.height,
-      8,
-      "aquamarine"
-    )
-  );
-}
-
 function IncreaseScore() {
   if (progressBar.value > 0) {
     let pointIncrease = 10 * scoreMultiplier;
@@ -321,12 +261,14 @@ function IncreaseScore() {
     document.getElementById("score").innerHTML = points;
   }
 }
-function IncreaseTime() {
-  if (progressBar.value > 0) {
-    time++;
-  };
-};
 
+const spawnEnemies = setInterval(spawnEnemy, 3000);
+const spawnScoreFactors = setInterval(spawnScoreFactor, 5000);
+const spawnEnemyErasers = setInterval(spawnEnemyEraser, 9000);
+const spawnHealthRecovery = setInterval(spawnHealth, 30000);
+const score = setInterval(IncreaseScore, 1000);
+
+requestAnimationFrame(drawScene);
 function restartGame() {
   if (progressBar.value === 0) {
     progressBar.value = 100;
@@ -335,14 +277,5 @@ function restartGame() {
   }
 }
 
-const spawnEnemies = setInterval(spawnEnemy, 3000);
-const spawnScoreFactors = setInterval(spawnScoreFactor, 5000);
-const spawnEnemyErasers = setInterval(spawnEnemyEraser, 4000);
-const spawnHealths = setInterval(spawnHealth, 4500);
-// const spawnFreezes = setInterval(spawnFreeze, 5500);
-const score = setInterval(IncreaseScore, 1000);
-const addSeconds = setInterval(IncreaseTime,1000);
-
-
 button.addEventListener("click", restartGame);
-requestAnimationFrame(drawScene);
+   
